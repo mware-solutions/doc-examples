43b913d622182f8d7911a4749a7941250ad5b695
Flavius Burca <flaviusburca+flaviusburca@users.noreply.github.com>
2018-11-02 12:10:01 +0200

    typo

--- extension-dashboard-toolbaritem/src/main/resources/com/mware/examples/dashboard_toolbaritem/plugin.js
+++ extension-dashboard-toolbaritem/src/main/resources/com/mware/examples/dashboard_toolbaritem/plugin.js
@@ -6,7 +6,7 @@ require(['public/v1/api'], function(bc) {
             return options.extension.identifier === 'com.mware-web-notifications'
         },
         tooltip: 'My Example Action',
-        icon: '/com.mware/examples/dashboard_toolbaritem/trash.png',
+        icon: '/com/mware/examples/dashboard_toolbaritem/trash.png',
         action: {
             type: 'popover',
             componentPath: 'com/mware/examples/dashboard_toolbaritem/popover'
